a2317ad2b4cf63d04b42d45ab99f87f8007857c2
AnSt <starininandrey@gmail.com>
2021-06-24 14:27:16 +0300

    Update

--- Test/TestClass.cs
+++ Test/TestClass.cs
@@ -2,7 +2,42 @@
 
 namespace Test
 {
+    /// <summary>
+    /// Тестовый класс для изучения автодокументирования
+    /// </summary>
     public class TestClass
     {
+        /// <summary>
+        /// Метод приветствия
+        /// </summary>
+        /// <remarks>
+        /// Статический метод.
+        /// </remarks>
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome!");
+        }
+
+        /// <summary>
+        /// Сложение числа <paramref name="num1"/> с числом <paramref name="num2"/> и возврат их суммы.
+        /// </summary>
+        /// <param name="num1">
+        /// первое целое число
+        /// </param>
+        /// <param name="num2">
+        /// Второе целое число
+        /// </param>
+        /// <returns>
+        /// Возвращает сумму двух целых чисел
+        /// </returns>
+        /// <example>
+        /// <code>
+        /// var result = TestClass.Add(5, 6);
+        /// </code>
+        /// </example>
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
     }
 }
